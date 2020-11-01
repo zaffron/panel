17e9b1b4c9a908ec76246b52641ab66e28d45279
Avinash Rijal <avinashrijal@gmail.com>
2020-11-01 16:57:39 +0545

    fixed eslint errors

---
+++ resources/scripts/components/dashboard/CoinsEarnContainer.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import ContentBox from '@/components/elements/ContentBox';
+import PageContentBlock from '@/components/elements/PageContentBlock';
+import tw from 'twin.macro';
+import { breakpoint } from '@/theme';
+import styled from 'styled-components/macro';
+import { useState } from 'react';
+
+const Container = styled.div`
+    ${tw`flex flex-wrap my-10`};
+
+    & > div {
+        ${tw`w-full`};
+
+        ${breakpoint('md')`
+            width: calc(50% - 1rem);
+        `}
+
+        ${breakpoint('xl')`
+            ${tw`w-auto flex-1`};
+        `}
+    }
+`;
+
+export default () => {
+    const totalCoinsEarned = useState(0);
+
+    return (
+        <PageContentBlock title={'Coins Overview'}>
+            <Container>
+                <ContentBox css={tw`w-full py-4 px-8 shadow-lg rounded-lg my-20`} title={'Earn Coins'}>
+                    <div css={tw`flex justify-center md:justify-end -mt-16`}>
+                        <img css={tw`w-20 h-20 object-cover rounded-full shadow-lg`} src="/images/logo.png" />
+                    </div>
+                    <div css={tw`text-center`}>
+                        <h2 css={tw`text-white text-3xl font-bold`}>Netricnodes</h2>
+                        <p css={tw`mt-2 text-white`}>
+                            You are currently earning Nitros. Keep this page open in your browser to earn more Nitros.
+                        </p>
+                        <hr css={tw`my-5`} />
+                        <p css={tw`mt-2 text-white`}>
+                            You have earned
+                            <span css={tw`text-green-500 mx-4 font-extrabold text-lg`}>{totalCoinsEarned}</span>
+                            Nitros during this session.
+                        </p>
+                    </div>
+                    <hr css={tw`my-5`} />
+                    <div css={tw`flex flex-col justify-center mt-4`}>
+                        <h3 css={tw`text-2xl font-normal mb-3 leading-7`}>Notes</h3>
+                        <p css={tw`text-white`}>- {'Don\'t'} open this page multiple times.</p>
+                        <p css={tw`text-white`}>- Make sure you {'don\'t'} have any Ad Blocker activated.</p>
+                        <p css={tw`text-white`}>
+                            - Make sure you {'didn\'t'} disable WebRTC in your browser
+                            settings.
+                        </p>
+                        <p css={tw`text-white`}>
+                            - Make sure you are not opted out of Arc.io. You can check
+                            that by clicking the blue icon in the bottom left corner.
+                        </p>
+                    </div>
+                </ContentBox>
+            </Container>
+        </PageContentBlock>
+    );
+};
---
+++ resources/scripts/components/dashboard/CoinsOverviewContainer.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import ContentBox from '@/components/elements/ContentBox';
+import PageContentBlock from '@/components/elements/PageContentBlock';
+import tw from 'twin.macro';
+import { breakpoint } from '@/theme';
+import styled from 'styled-components/macro';
+import { faCoins } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+
+const Container = styled.div`
+    ${tw`flex flex-wrap my-10`};
+
+    & > div {
+        ${tw`w-full`};
+
+        ${breakpoint('md')`
+            width: calc(50% - 1rem);
+        `}
+
+        ${breakpoint('xl')`
+            ${tw`w-auto flex-1`};
+        `}
+    }
+`;
+
+export default () => {
+    return (
+        <PageContentBlock title={'Coins Overview'}>
+            <Container>
+                <ContentBox css={tw`w-full`} title={'Nitro'}>
+                    <h3 css={tw`text-2xl font-bold mb-3 leading-7`}>What is nitro?</h3>
+                    <p>
+                        {'Nitro'} is our own currency that you can use to exchange into Nodes resources.
+                        You can earn some Nitros by referring friends, keeping your browser open on our website, or following certain earning methods mentioned here.
+                        {'It\'s'} always free to earn {'Nitros'} and we {'won\'t'} charge any cost for it.
+                    </p>
+                </ContentBox>
+                <ContentBox css={tw`xl:ml-8 mt-8 xl:mt-0`} title={'Netric Coins'}>
+                    <div css={tw`flex items-start px-4 py-6 w-full`}>
+                        <span css={tw`w-12 h-12 rounded-full object-cover mr-4 shadow`}>
+                            <span css={tw`flex items-center justify-center h-full`}>
+                                <FontAwesomeIcon icon={faCoins} />
+                            </span>
+                        </span>
+                        <div css={tw`w-full`}>
+                            <div css={tw`flex items-center justify-between mb-3`}>
+                                <h2
+                                    css={tw`text-lg font-semibold text-white -mt-1`}
+                                >
+                                    Total Nitros:
+                                </h2>
+                                <span css={tw`text-green-500 font-black`}>0 🚀</span>
+                            </div>
+                            <div css={tw`flex items-center justify-between`}>
+                                <p css={tw`text-white`}>Last earned:</p>
+                                <p css={tw`text-white`}>
+                                    12 Sep 2012
+                                </p>
+                            </div>
+                            <div css={tw`mt-4 flex items-center justify-between`}>
+                                <p css={tw`text-white`}>Want to get more nitros?</p>
+                                <div css={tw`flex text-white text-sm`}>
+                                    <Link
+                                        to="/coins/earn"
+                                        css={tw`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded`}
+                                    >
+                                        Earn more
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </ContentBox>
+            </Container>
+        </PageContentBlock>
+    );
+};
